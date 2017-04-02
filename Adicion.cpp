9c523d6d4564626d4aa8165a132cfcb6b7e03a0b
jpwierna <juanpa.933@gmail.com>
2017-04-02 14:07:39 -0300

    Add files via upload

---
+++ Adicion.cpp
@@ -0,0 +1,24 @@
+/*
+TP #1 Adicion
+02/04/2017
+Wierna Juan Pablo
+*/
+
+#include<iostream>
+
+
+int main()
+{
+
+	int a;
+	int b;
+
+
+	std::cout << "Ingrese un numero\n";
+	std::cin >> a;
+	std::cout << "Ingrese otro numero\n";
+	std::cin >> b;
+	std::cout << "La suma es igual a " << a + b << std::endl;
+
+
+}
